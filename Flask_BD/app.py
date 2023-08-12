2cf3f0d3a5bfef3158233f4145723dc320e52b76
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-08-12 14:43:44 -0500

    Python Api-Rest

--- Flask_BD/app.py
+++ Flask_BD/app.py
@@ -1,10 +1,11 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template,request,redirect,url_for,jsonify
 app= Flask(__name__)
 
 from sqlalchemy import create_engine
 #Object Relational Mapping
 from sqlalchemy.orm  import sessionmaker
 from Crear_bd import Base,Book
+
 
 
 #Conectarme a la base de datos #Bind = Enlace a una BD existente
@@ -64,16 +65,65 @@ def editBook(book_id):
         return render_template('editBook.html',book=bookToEdit)
 
 
-        
+#Api Rest
+# Funciones para las Apis
+def get_books():
+    books = session.query(Book).all()
+    return jsonify(books=[b.serialize for b in books])
+
+def book_new(titulo,autor,genero,precio):
+    newBook=Book(title=titulo,
+                     author=autor,
+                     genre=genero,
+                     price=precio)
+    session.add(newBook)
+    session.commit()
+    return jsonify(Book=newBook.serialize)
+
+def get_book(book_id):
+    book=session.query(Book).filter_by(id=book_id).one()
+    return jsonify(books=book.serialize)
+
+def deleteBook(id):
+     bookToDelete=session.query(Book).filter_by(id=id).one()
+     session.delete(bookToDelete)
+     session.commit()
+     return 'Book removido con exito %s' % id
+
+def updateBook(id,title):
+    bookupdate=session.query(Book).filter_by(id=id).one()
+    if not title:
+        bookupdate.title=title
+    session.add(bookupdate)    
+    session.commit()
+    return 'Book Actualizado con exito %s' % id
+     
+
+
+#Crear los Endpoints
+@app.route('/booksApi',methods=['GET','POST'])
+def booksFunction():
+    if request.method == 'GET':
+        return get_books()
+    elif request.method =='POST':
+        title=request.args.get('title','')
+        author=request.args.get('author','')
+        genre=request.args.get('genre','')
+        price=request.args.get('price','')
+        return book_new(title,author,genre,price)
+    
+@app.route('/booksApi/<int:id>',methods=['GET','PUT','DELETE'])    
+def bookfunctionid(id):
+    if request.method=='GET':
+        return get_book(id)
+    elif request.method =='DELETE':
+        return deleteBook(id)
+    elif request.method=='PUT':
+         title=request.args.get('title','')
+         return updateBook(id,title)
+    
 
         
-                     
-                     
-                     
-
-
-
-
 if __name__ == '__main__':
     app.debug=True
     app.run(host='0.0.0.0',port=4996)
